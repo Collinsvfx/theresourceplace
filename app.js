b3ae9625144ce28edd6092c5e577b67ccaf3f157
Collins Mogbolu <Collinsvfx@gmail.com>
2022-11-22 06:16:09 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const modalBtn = document.querySelector(".h-burger-container");
+const modalContainer = document.querySelector(".modal-container");
+const cancelBtn = document.querySelector(".cancel-btn-container");
+const groupTaskBtn = document.querySelector(".g-task");
+const personalTaskBtn = document.querySelector(".p-task ");
+
+modalBtn.addEventListener("click", function () {
+  console.log(modalContainer);
+  modalContainer.style.display = "block";
+});
+
+cancelBtn.addEventListener("click", function () {
+  modalContainer.style.display = "none";
+});
+
+groupTaskBtn.addEventListener("click", function () {
+  groupTaskBtn.classList.replace("g-task", "g-task-updated");
+  personalTaskBtn.classList.replace("p-task", "p-task-updated");
+});
+
+personalTaskBtn.addEventListener("click", function () {
+  if (personalTaskBtn.classList.contains("p-task-updated")) {
+    personalTaskBtn.classList.replace("p-task-updated", "p-task");
+    groupTaskBtn.classList.replace("g-task-updated", "g-task");
+  }
+});
