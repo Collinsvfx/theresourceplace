7118f085127aee54bd7320c8e0b628969320ea51
Collinsvfx <collinsvfx@gmail.com>
2022-11-23 08:14:46 +0100

    log in funvtionality

--- app.js
+++ app.js
@@ -6,6 +6,52 @@ const cancelBtn = document.querySelector(".cancel-btn-container");
 const groupTaskBtn = document.querySelector(".g-task");
 const personalTaskBtn = document.querySelector(".p-task");
 const submittedBtn = document.querySelector(".submitted-task-btn");
+const userId = document.getElementById("your-ID");
+const userPassword = document.getElementById("your-Password");
+const loginBtn = document.querySelector(".loginin-btn");
+
+// USERS
+const user1 = {
+  userID: 13041997,
+  password: "collins5670",
+};
+
+const user2 = {
+  userID: 13041995,
+  password: 123456789,
+};
+
+const accounts = [user1, user2];
+
+let theUserIds = [];
+let theUserPass = [];
+
+const userIds = accounts.forEach((id) => {
+  theUserIds.push(id.userID);
+});
+
+const userPass = accounts.forEach((pass) => {
+  theUserPass.push(String(pass.password));
+});
+
+// Login checker
+function userCheck() {
+  for (let i = 0; i < theUserIds.length; i++) {
+    for (let p = 0; p < theUserPass.length; p++) {
+      const y = userPassword.value.toLocaleLowerCase();
+
+      if (
+        theUserIds[i] === Number(userId.value) &&
+        theUserPass[p] === String(y)
+      ) {
+        // console.log("asasa");
+        location.href = "welcome_screen.html";
+      }
+    }
+  }
+}
+
+loginBtn.addEventListener("click", userCheck);
 
 modalBtn.addEventListener("click", function () {
   console.log(modalContainer);
